d23b2d8afe4630823123a56c1fa30f5c3affefd1
thilak <thilakal9299@gmail.com>
2018-08-24 00:49:39 +0530

    handled auth

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+    mongoURI:'mongodb://thilak:crypers1@ds231242.mlab.com:31242/storybooks-crypers',
+    googleClientID:'137091079134-4q4c163d7kmife1a0blerqps5nj0j4es.apps.googleusercontent.com',
+    googleClientSecret:'ripAf3--JR-XcYpLEDN4q8Xj'
+
+}
