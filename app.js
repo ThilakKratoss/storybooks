d10012834229a23133cf42032537587b3729535e
thilak <thilakal9299@gmail.com>
2018-08-24 00:43:52 +0530

    handled oauth

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
+
+
+//passport
+require('./config/passport')(passport);
+
+//load routes
+const auth = require('./routes/auth');
 
 const app = express();
-
 app.get('/',(req,res)=>{
     res.send('hi its works');
 })
+
+//use auth
+app.use('/auth',auth);
 
 const port = process.env.PORT || 4500;
 app.listen(port,()=>{
     console.log('server started');
 });
+
+
+
+
